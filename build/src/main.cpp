5a6f1c6f94eeda1a7e685a8424d104cb1b8ecb9c
adrian-mb <adrian.mb.fiub@gmail.com>
2017-08-12 23:08:05 -0300

    Implement webserver

--- build/src/main.cpp
+++ build/src/main.cpp
@@ -1,2 +1,41 @@
+#include "webserver/HttpHandler.h"
+#include "webserver/HttpRequest.h"
+#include "webserver/HttpResponse.h"
+
+#include "webserver/Server.h"
+
+#include <sstream>
+
+static const char *port = "8000";
+
+namespace rest {
+
+class SimpleHandler : public HttpHandler {
+public:
+
+    void handle(
+            HttpRequest const &request,
+            HttpResponse &response,
+            PathVariables &pathvars) {
+
+        std::stringstream ss;
+
+        ss << request.GetUri();
+        ss << ": Hello, " << pathvars["name"] << "!";
+
+        response.SetStatus(200);
+        response.SetBody(ss.str());
+    }
+
+};
+
+}
+
 int main() {
+    rest::SimpleHandler handler;
+
+    rest::Get("/v1/api/greet/:name", handler);
+
+    rest::StartServer(port);
+    return 0;
 }
